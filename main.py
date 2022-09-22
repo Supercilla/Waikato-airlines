d011c4e2b66871bc01631e26e633a2e8bd7cc275
Supercilla <>
2022-09-22 21:45:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#imports 
+import random
+import re
+
+#variables
+wellington = 110
+christchurch = 140
+napier = 80
+rotorua = 80
+dunedin = 105
+discount = round(random.uniform(0.09, 0.26), 2)
+
+print(discount)
